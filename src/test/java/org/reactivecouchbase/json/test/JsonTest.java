70851ed5f952d74a427ebcc19e6898f34c745f9c
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2018-02-09 10:21:08 +0100

    Fix

--- src/test/java/org/reactivecouchbase/json/test/JsonTest.java
+++ src/test/java/org/reactivecouchbase/json/test/JsonTest.java
@@ -28,7 +28,7 @@ public class JsonTest {
 
         public String getName() {
             return name;
-        }
+        } 
 
         public void setName(String name) {
             this.name = name;
